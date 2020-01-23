a500b9c6ddbb1980cc04d05d5e3752d1c848158f
Julian Rüth <julian.rueth@fsfe.org>
2020-01-23 21:12:27 +0100

    enable CI

---
+++ recipe/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+source $RECIPE_DIR/environment.sh
+
+$SNIPPETS_DIR/autoconf/run.sh
+$SNIPPETS_DIR/make/run.sh
+
+$SNIPPETS_DIR/clang-format/run.sh
+$SNIPPETS_DIR/asv/run.sh
+$SNIPPETS_DIR/todo/run.sh
+$SNIPPETS_DIR/codecov/run.sh
